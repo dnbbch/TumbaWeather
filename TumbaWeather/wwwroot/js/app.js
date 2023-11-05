b4ec4e799246ea38b048cee5d95e76f14ee06d72
dnbbch <daniil0406bbch@gmail.com>
2023-11-05 23:03:33 +0800

    Исправленная версия проекта

--- TumbaWeather/wwwroot/js/app.js
+++ TumbaWeather/wwwroot/js/app.js
@@ -9,17 +9,77 @@ async function getWeather() {
       headers: { Accept: "application/json" },
     }
   );
+
+  // Очищаем контейнер перед добавлением новых карточек
+  const cardContainer = document.getElementById("card-container");
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
+  }
+
   // Если запрос прошел нормально
   if (response.ok === true) {
     // Получаем данные
-    const result = await response.json();
-    console.log("City");
-    console.log(result.result.city.name);
-    console.log(result);
+    const data = await response.json();
 
-    // Обновляем текст в элементе city-title
-    var cityTitleElement = document.getElementById("city-title");
-    cityTitleElement.textContent = result.result.city.name;
+    // Обновляем город и страну
+    const cityTitleElement = document.getElementById("city-title");
+    const countryTitleElement = document.getElementById("country-title");
+
+    // Обновляем содержимое элемента aside__header
+    cityTitleElement.innerHTML = `${data.result.city.name}<span id="country-title">${data.result.city.country}</span>`;
+
+    // Обновляем страну
+    countryTitleElement.textContent = data.result.city.country;
+
+    // Обновляем текст в карточках
+    const cardContainer = document.getElementById("card-container");
+
+    for (let i = 0; i < data.result.list.length / 8; i++) {
+      const card = document.createElement("div");
+      card.className = "article__card";
+
+      const cardDateElement = document.createElement("p");
+      cardDateElement.className = "card__date";
+
+      const cardHeader = document.createElement("div");
+      cardHeader.className = "card__header";
+
+      const cardIconsElement = document.createElement("div");
+      cardIconsElement.className = "card__icons";
+
+      card.appendChild(cardDateElement);
+      card.appendChild(cardHeader);
+      cardHeader.appendChild(cardIconsElement);
+
+      cardContainer.appendChild(card);
+
+      const forecastData = data.result.list.slice(i * 8, (i + 1) * 8);
+      cardDateElement.textContent = forecastData[0].dt_txt.split(" ")[0];
+
+      forecastData.forEach((weatherData) => {
+        const dateTime = new Date(weatherData.dt_txt);
+        const temperature = Math.round(weatherData.main.temp);
+        const iconCode = weatherData.weather[0].icon;
+
+        const tempElement = document.createElement("p");
+        tempElement.className = "card__temp";
+        tempElement.innerHTML = `${dateTime.toLocaleTimeString("ru-RU", {
+          hour: "numeric",
+          minute: "numeric",
+        })} | ${temperature} °C`;
+
+        const iconElement = document.createElement("img");
+        iconElement.src = `../icons/fill/${iconCode}.svg`;
+        iconElement.alt = "Weather Icon";
+
+        const tempIconContainer = document.createElement("div");
+        tempIconContainer.className = "temp-icon-container";
+        tempIconContainer.appendChild(tempElement);
+        tempIconContainer.appendChild(iconElement);
+
+        cardHeader.appendChild(tempIconContainer);
+      });
+    }
   }
 }
 //Метод для запроса с сервера данных, относительно координат
@@ -31,24 +91,82 @@ async function getWeatherByCoord(lat, lon) {
       headers: { Accept: "application/json" },
     }
   );
+
+  // Очищаем контейнер перед добавлением новых карточек
+  const cardContainer = document.getElementById("card-container");
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
+  }
+
   // Если запрос прошел нормально
   if (response.ok === true) {
     // Получаем данные
-    const result = await response.json();
-
-    // Перекинуть
-    console.log("Coordinate");
-    console.log(result.result.city.name);
-    console.log(result);
+    const data = await response.json();
 
     // Обновляем текст в элементе city-title
-    var cityTitleElement = document.getElementById("city-title");
-    cityTitleElement.textContent = result.result.city.name;
+    const cityTitleElement = document.getElementById("city-title");
+    const countryTitleElement = document.getElementById("country-title");
+
+    // Обновляем содержимое элемента aside__header
+    cityTitleElement.innerHTML = `${data.result.city.name}<span id="country-title">${data.result.city.country}</span>`;
+
+    // Обновляем страну
+    countryTitleElement.textContent = data.result.city.country;
+
+    // Обновляем текст в карточках
+    const cardContainer = document.getElementById("card-container");
+
+    for (let i = 0; i < data.result.list.length / 8; i++) {
+      const card = document.createElement("div");
+      card.className = "article__card";
+
+      const cardDateElement = document.createElement("p");
+      cardDateElement.className = "card__date";
+
+      const cardHeader = document.createElement("div");
+      cardHeader.className = "card__header";
+
+      const cardIconsElement = document.createElement("div");
+      cardIconsElement.className = "card__icons";
+
+      card.appendChild(cardDateElement);
+      card.appendChild(cardHeader);
+      cardHeader.appendChild(cardIconsElement);
+
+      cardContainer.appendChild(card);
+
+      const forecastData = data.result.list.slice(i * 8, (i + 1) * 8);
+      cardDateElement.textContent = forecastData[0].dt_txt.split(" ")[0];
+
+      forecastData.forEach((weatherData) => {
+        const dateTime = new Date(weatherData.dt_txt);
+        const temperature = Math.round(weatherData.main.temp);
+        const iconCode = weatherData.weather[0].icon;
+
+        const tempElement = document.createElement("p");
+        tempElement.className = "card__temp";
+        tempElement.innerHTML = `${dateTime.toLocaleTimeString("ru-RU", {
+          hour: "numeric",
+          minute: "numeric",
+        })} | ${temperature}°C`;
+
+        const iconElement = document.createElement("img");
+        iconElement.src = `../icons/fill/${iconCode}.svg`;
+        iconElement.alt = "Weather Icon";
+
+        const tempIconContainer = document.createElement("div");
+        tempIconContainer.className = "temp-icon-container";
+        tempIconContainer.appendChild(tempElement);
+        tempIconContainer.appendChild(iconElement);
+
+        cardHeader.appendChild(tempIconContainer);
+      });
+    }
   }
 }
 document
   .getElementById("sendButtonId")
-    .addEventListener("click", () => getWeather());
+  .addEventListener("click", () => getWeather());
 navigator.geolocation.getCurrentPosition(function (position) {
   getWeatherByCoord(position.coords.latitude, position.coords.longitude);
 });
